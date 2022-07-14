ba812ed82d666e9d782e9f6edb56a822c7251d9f
Apoorva Choudhary <apoorvapkc@gmail.com>
2022-07-14 23:02:51 +0530

    main code

---
+++ src/main/java/com/tgt/rysetii/learningesourcesapi/LearningesourcesApiApplication.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetii.learningesourcesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningesourcesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningesourcesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tgt/rysetii/learningesourcesapi/LearningesourcesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetii.learningesourcesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningesourcesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
